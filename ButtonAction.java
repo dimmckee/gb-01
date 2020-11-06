4dd080bf0b01050ba2d27e035e627349cbedd082
dimmckee <68608257+dimmckee@users.noreply.github.com>
2020-11-07 01:16:34 +0300

    Add files via upload

---
+++ ButtonAction.java
@@ -0,0 +1,11 @@
+package HM004;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonAction implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Check");
+    }
+}
